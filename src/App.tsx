abccdebd0bc6ccdc5733e785ea6033016d052dfd
Matheus Sunderhus <matheus.sunderhus@gmail.com>
2021-02-22 16:25:44 -0300

    feature: Create experience bar.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
+
+import ExperienceBar from './components/ExperienceBar';
+import './styles/global.css';
+
 function App() {
   return (
-    <h1>hello NLW</h1>
+    <div className="container">
+      <ExperienceBar />
+    </div>
   );
 }
 
---
+++ src/components/ExperienceBar/index.tsx
@@ -0,0 +1,18 @@
+import './styles.css';
+
+const ExperienceBar: React.FC = () => {
+    return (
+        <header className="experience-bar">
+            <span> 0 xp</span>
+            <div>
+                <div style={{ width: '50%' }} />
+                <span className="current-experience" style={{left:'50%'}}>
+                    300xp
+                </span>
+            </div>
+            <span>600xp</span>
+        </header>
+    );
+}
+
+export default ExperienceBar;
